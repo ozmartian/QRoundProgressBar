44e30877f1b108bff46d56b133be93b1daeac94e
Pete Alexandrou <pete@ozmartians.com>
2017-04-17 17:21:59 +0800

    added licensing comment headers

--- QRoundProgressBar.py
+++ QRoundProgressBar.py
@@ -1,16 +1,43 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+#######################################################
+#
+# Copyright 2017 Pete Alexandrou
+#
+# Ported to Python from the original works in C++ by:
+#
+#     Sintegrial Technologies (c) 2015
+#     https://sourceforge.net/projects/qroundprogressbar
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#######################################################
+
 import operator
 from enum import Enum
 
 from PyQt5.QtCore import pyqtSlot, QPointF, Qt, QRectF
-from PyQt5.QtGui import (QPalette, QConicalGradient, QGradient, QRadialGradient, QFontMetricsF, QFont, QPainter, QPen,
-                         QPainterPath, QImage, QPaintEvent)
+from PyQt5.QtGui import (QPalette, QConicalGradient, QGradient, QRadialGradient,
+                         QFontMetricsF, QFont, QPainter, QPen, QPainterPath, QImage,
+                         QPaintEvent)
 from PyQt5.QtWidgets import QWidget
 
 
 class QRoundProgressBar(QWidget):
+
+    # CONSTANTS
+
     PositionLeft = 180
     PositionTop = 90
     PositionRight = 0
